e92bb755834377962c9eb395d95a790455064ff1
Oghenero Ekwevugbe <nero1deluxe@gmail.com>
2020-12-11 07:59:18 -0500

    Adding jenkins logs script

--- Scripts/jenkins_logs.sh
+++ Scripts/jenkins_logs.sh
@@ -2,7 +2,7 @@
 
 # AUTHOR: Oghenero Ekwevugbe
 # PURPOSE: This script turns the last line of jenkins logs in docker container and send out to output.log in current directory
-# FURTHER INSTRUSTION: Schedule script to run as a cronjob (* * * * * /home/<username>/script/jenkins_logs.sh)
+# FURTHER INSTRUSTION: Schedule script to run as a cronjob (crontab -e => * * * * * /home/<username>/script/jenkins_logs.sh)
 # DATE MODIFIED: 12/10/2020
 
 CONTAINER_ID='532a135a747b'
