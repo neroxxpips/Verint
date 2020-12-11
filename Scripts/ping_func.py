1165bd1c0f72af8e19e8a791252589f08a4cb1d0
Oghenero Ekwevugbe <nero1deluxe@gmail.com>
2020-12-11 10:40:51 -0500

    Adding python aws lambda script

---
+++ Scripts/ping_func.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+'''
+This function ping google.com for every 10 secs for 5 minutes continuously.
+
+'''
+from pythonping import ping
+import time
+import json
+import socket
+
+def ping_server(event, context): # to test code outside of aws lambda remove both arguments(event, context)
+    
+    cnt = 1
+    host = 'www.google.com'
+
+    initial_min = 0
+    duration = 300 #(5 * 60 )
+
+    try:
+        while initial_min < duration:
+            
+            ip = socket.gethostbyname(host)
+            ping(ip, count=1)
+            print("www.google.com is Online!")
+            print(f"{cnt} Waiting for 10 secs...")
+            time.sleep(10) # sleeps for 10 secs
+            initial_min += 10
+            cnt += 1
+
+    except socket.error:
+        print(host, 'Offline')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Ping Google Server Successfully!')
+    }
+    
+
+
+        
+
+
